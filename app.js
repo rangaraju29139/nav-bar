bed8853de6f0e8a345709ad91b99f2eec1aeecf7
rangaraju29139 <rangaraju29139@gmail.com>
2022-07-27 12:26:37 +0530

    added the show-links toggle functionality using add and remove methods of classList

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 // classList - shows/gets all classes
+
 // contains - checks classList for specific class
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+navToggle.addEventListener("click", () => {
+  console.log(links.classList);
+  const list = links.classList;
+
+  if (list.contains("show-links")) {
+    list.remove("show-links");
+  } else {
+    list.add("show-links");
+  }
+});
